f1eb49c1020406195486354714e00e3613562c2c
Jose Pablo Gonzalez Alvarado <jose51197@estudiantec.cr>
2019-11-03 18:24:30 -0600

    Base home window and skeleton for the configuration

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("View/mainwindow.fxml"));
+        primaryStage.setTitle("Asistente Inclusiones ATI");
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
