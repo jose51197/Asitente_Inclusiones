b3ea726c36181eb11700f35ec4eb39a40d5f6bcb
Sergie <sergie98@gmail.com>
2019-11-11 22:36:19 -0600

    arreglos en el main

--- src/Controllers/CargadorArchivos.java
+++ src/Controllers/CargadorArchivos.java
@@ -1,5 +1,94 @@
 package Controllers;
 
+import Model.CSVreader;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class CargadorArchivos {
+    private CSVreader reader= new CSVreader();
+
+    void serializeObject( Object object){
+        /*try {
+            FileOutputStream fileOut = new FileOutputStream("plan.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(malla);
+            out.close();
+            fileOut.close();
+            fileOut = new FileOutputStream("estudiantes.ser");
+            out = new ObjectOutputStream(fileOut);
+            out.writeObject(estudiantes);
+            out.close();
+            fileOut.close();
+            fileOut = new FileOutputStream("aulas.ser");
+            out = new ObjectOutputStream(fileOut);
+            out.writeObject(aulas);
+            out.close();
+            fileOut.close();
+            fileOut = new FileOutputStream("grupos.ser");
+            out = new ObjectOutputStream(fileOut);
+            out.writeObject(grupos);
+            out.close();
+            fileOut.close();
+            fileOut = new FileOutputStream("inclusionesMap.ser");
+            out = new ObjectOutputStream(fileOut);
+            out.writeObject(inclusionesMap);
+            out.close();
+            fileOut.close();
+            fileOut = new FileOutputStream("inclusiones.ser");
+            out = new ObjectOutputStream(fileOut);
+            out.writeObject(inclusiones);
+            out.close();
+            fileOut.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+        /*Map<String, Curso> malla;
+        Map<Integer, Estudiante> estudiantes;
+        Map<String, Aula> aulas;
+        Map<String, Grupo> grupos;
+        Map <Integer,ArrayList<Inclusion>> inclusionesMap;
+        ArrayList<Inclusion> inclusiones;
+        try {
+            FileInputStream fileIn = new FileInputStream("plan.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            malla = (Map<String, Curso>) in.readObject();
+            in.close();
+            fileIn.close();
+            fileIn = new FileInputStream("aulas.ser");
+            in = new ObjectInputStream(fileIn);
+            aulas = (Map<String, Aula>) in.readObject();
+            in.close();
+            fileIn.close();
+            fileIn = new FileInputStream("estudiantes.ser");
+            in = new ObjectInputStream(fileIn);
+            estudiantes = (Map<Integer, Estudiante> ) in.readObject();
+            in.close();
+            fileIn.close();
+            fileIn = new FileInputStream("grupos.ser");
+            in = new ObjectInputStream(fileIn);
+            grupos = (Map<String, Grupo>) in.readObject();
+            in.close();
+            fileIn.close();
+            fileIn = new FileInputStream("inclusionesMap.ser");
+            in = new ObjectInputStream(fileIn);
+            inclusionesMap = (Map <Integer,ArrayList<Inclusion>> ) in.readObject();
+            in.close();
+            fileIn.close();
+            fileIn = new FileInputStream("inclusiones.ser");
+            in = new ObjectInputStream(fileIn);
+            inclusiones = (ArrayList<Inclusion> ) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+            return;
+        } catch (ClassNotFoundException c) {
+            System.out.println("Employee class not found");
+            c.printStackTrace();
+            return;
+        }*/
+    }
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -32,6 +32,11 @@ public class Main extends Application {
     }
 
     public static void pruebasSergie() throws IOException {
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
         CSVreader r= new CSVreader();
         Map<String, Curso> malla=r.getMalla_Curricular("plan.csv");
         Map<Integer, Estudiante> estudiantes=r.getEstudiantes("rn.csv",malla);
@@ -43,92 +48,10 @@ public class Main extends Application {
         estudiantes.put(2016157695,jose);
         Map <Integer,ArrayList<Inclusion>> inclusionesMap= new HashMap<Integer, ArrayList<Inclusion>>();
         ArrayList<Inclusion> inclusiones = r.getInclusiones("inclusiones.csv",grupos,estudiantes,inclusionesMap);
-        try {
-            FileOutputStream fileOut = new FileOutputStream("plan.ser");
-            ObjectOutputStream out = new ObjectOutputStream(fileOut);
-            out.writeObject(malla);
-            out.close();
-            fileOut.close();
-            fileOut = new FileOutputStream("estudiantes.ser");
-            out = new ObjectOutputStream(fileOut);
-            out.writeObject(estudiantes);
-            out.close();
-            fileOut.close();
-            fileOut = new FileOutputStream("aulas.ser");
-            out = new ObjectOutputStream(fileOut);
-            out.writeObject(aulas);
-            out.close();
-            fileOut.close();
-            fileOut = new FileOutputStream("grupos.ser");
-            out = new ObjectOutputStream(fileOut);
-            out.writeObject(grupos);
-            out.close();
-            fileOut.close();
-            fileOut = new FileOutputStream("inclusionesMap.ser");
-            out = new ObjectOutputStream(fileOut);
-            out.writeObject(inclusionesMap);
-            out.close();
-            fileOut.close();
-            fileOut = new FileOutputStream("inclusiones.ser");
-            out = new ObjectOutputStream(fileOut);
-            out.writeObject(inclusiones);
-            out.close();
-            fileOut.close();
-        } catch (IOException i) {
-            i.printStackTrace();
-        }
-        /*Map<String, Curso> malla;
-        Map<Integer, Estudiante> estudiantes;
-        Map<String, Aula> aulas;
-        Map<String, Grupo> grupos;
-        Map <Integer,ArrayList<Inclusion>> inclusionesMap;
-        ArrayList<Inclusion> inclusiones;
-        try {
-            FileInputStream fileIn = new FileInputStream("plan.ser");
-            ObjectInputStream in = new ObjectInputStream(fileIn);
-            malla = (Map<String, Curso>) in.readObject();
-            in.close();
-            fileIn.close();
-            fileIn = new FileInputStream("aulas.ser");
-            in = new ObjectInputStream(fileIn);
-            aulas = (Map<String, Aula>) in.readObject();
-            in.close();
-            fileIn.close();
-            fileIn = new FileInputStream("estudiantes.ser");
-            in = new ObjectInputStream(fileIn);
-            estudiantes = (Map<Integer, Estudiante> ) in.readObject();
-            in.close();
-            fileIn.close();
-            fileIn = new FileInputStream("grupos.ser");
-            in = new ObjectInputStream(fileIn);
-            grupos = (Map<String, Grupo>) in.readObject();
-            in.close();
-            fileIn.close();
-            fileIn = new FileInputStream("inclusionesMap.ser");
-            in = new ObjectInputStream(fileIn);
-            inclusionesMap = (Map <Integer,ArrayList<Inclusion>> ) in.readObject();
-            in.close();
-            fileIn.close();
-            fileIn = new FileInputStream("inclusiones.ser");
-            in = new ObjectInputStream(fileIn);
-            inclusiones = (ArrayList<Inclusion> ) in.readObject();
-            in.close();
-            fileIn.close();
-        } catch (IOException i) {
-            i.printStackTrace();
-            return;
-        } catch (ClassNotFoundException c) {
-            System.out.println("Employee class not found");
-            c.printStackTrace();
-            return;
-        }*/
-    }
 
-    public static void main(String[] args) throws IOException {
         pruebasJose();
         pruebasSergie();
         pruebasOscar();
-        //pruebas de funcionamiento de los diccionarios;
 
         //ResultPDF resultPDF = new ResultPDF();
         //resultPDF.write();
