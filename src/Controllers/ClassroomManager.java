52dca9c2f3c52429482393cd29eadf938ab89f39
Oscar Cortes <oscarcortesc12@gmail.com>
2019-11-12 04:04:03 -0600

    Combobox almacena toda la info de las aulas

--- src/Controllers/ClassroomManager.java
+++ src/Controllers/ClassroomManager.java
@@ -5,19 +5,23 @@ import Model.DataHolder;
 import Model.Grupo;
 import Model.Horario;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.GridPane;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class ClassroomManager {
 
     @FXML
     private GridPane gridHorario;
+    @FXML
+    private ComboBox combo_Aulas;
 
     private void fillGrid(){
-        Aula aula = DataHolder.getInstance().getAulas().get("B3-7");
+        Aula aula = DataHolder.getInstance().getAulas().get( (String) combo_Aulas.getValue());
 
         for (String key : DataHolder.getInstance().getGrupos().keySet()){
             Grupo grupo = DataHolder.getInstance().getGrupos().get(key);
@@ -37,6 +41,10 @@ public class ClassroomManager {
     }
 
     public void initialize(){
+        Set<String> aulas = DataHolder.getInstance().getAulas().keySet();
+        combo_Aulas.getItems().addAll(aulas);
+        combo_Aulas.getSelectionModel().selectFirst();
 
+        fillGrid();
     }
 }
